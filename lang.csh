669b6bab29add7b3a6d5c975d4c6be08f95c1bae
stangrze <stangrze@pld-linux.org>
1999-06-14 21:17:11 +0000

    Synced with latest from old repo
    git-svn-id: http://svn.pld-linux.org/svn/rc-scripts/trunk@369 11da6c6d-24c7-0310-97e0-b6d17e038e62

---
+++ lang.csh
@@ -0,0 +1,35 @@
+#!/bin/csh
+
+if [ -f /etc/sysconfig/i18n ]; then
+    . /etc/sysconfig/i18n
+
+    if [ -n "$LANG" ]; then
+	setenv LANG
+    fi
+
+    if [ -n "$LC_ALL" ]; then
+	setenv LC_ALL
+    fi
+  
+    if [ -n "$LINGUAS" ]; then
+	setenv LINGUAS
+    fi
+  
+    if [ -n "$SYSTERM" ]; then
+	setenv TERM=$SYSTERM
+    fi
+
+    # Set console font map.
+    if [ -n "$UNIMAP" ]; then
+	loadunimap $UNIMAP
+    fi
+
+    if [ -n "$SYSTERM" ] ; then
+	case $SYSTERM in
+	    linux-lat)
+		setenv LESSCHARSET=latin1
+		setenv INPUTRC=/etc/inputrc
+		;;
+	esac
+    fi
+fi
--- lang.sh
+++ lang.sh
@@ -3,34 +3,47 @@
 if [ -f /etc/sysconfig/i18n ]; then
     . /etc/sysconfig/i18n
 
-    if [ -n "$LANG" ]; then
-	export LANG
-    fi
+   [ -n "$LANG" ] && export LANG || unset LANG
+   [ -n "$LC_CTYPE" ] && export LC_CTYPE || unset LC_CTYPE
+   [ -n "$LC_COLLATE" ] && export LC_COLLATE || unset LC_COLLATE
+   [ -n "$LC_MESSAGES" ] && export LC_MESSAGES || unset LC_MESSAGES
+   [ -n "$LC_NUMERIC" ] && export LC_NUMERIC || unset LC_NUMERIC
+   [ -n "$LC_MONETARY" ] && export LC_MONETARY || unset LC_MONETARY
+   [ -n "$LC_TIME" ] && export LC_TIME || unset LC_TIME
+   [ -n "$LC_ALL" ] && export LC_ALL || unset LC_ALL
+   [ -n "$LANGUAGE" ] && export LANGUAGE || unset LANGUAGE
+   [ -n "$LINGUAS" ] && export LINGUAS || unset LINGUAS
 
-    if [ -n "$LC_ALL" ]; then
-	export LC_ALL
-    fi
-  
-    if [ -n "$LINGUAS" ]; then
-	export LINGUAS
-    fi
-  
-    if [ -n "$SYSTERM" ]; then
-	export TERM=$SYSTERM
-    fi
+   # deprecated
+   if [ -n "$SYSTERM" ]; then
+      export TERM=$SYSTERM
+   fi
 
-    # Set console font map.
-    if [ -n "$UNIMAP" ]; then
-	loadunimap $UNIMAP
-    fi
+   if [ -n "$SYSFONTACM" ]; then
+       case $SYSFONTACM in
+         iso01*|iso02*|iso15*|koi*)
+              LESSCHARSET=latin1
+	      INPUTRC=/etc/inputrc
+              export LESSCHARSET INPUTRC
+              if [ "$TERM" = "linux" ]; then
+                  if ls -l /proc/$$/fd/0 2>/dev/null | grep -- '-> /dev/tty[0-9]*$' >/dev/null 2>&1; then
+                      echo -n -e '\033(K' > /proc/$$/fd/0
+                  fi
+              fi
+              ;;
+      esac
+   fi
 
-    if [ -n "$SYSTERM" ] ; then
-	case $SYSTERM in
-	    linux-lat)
-		LESSCHARSET=latin1
-		INPUTRC=/etc/inputrc
-		export LESSCHARSET INPUTRC
-		;;
-	esac
-    fi
+   if [ -n "$INPUTRC" ]; then
+      export INPUTRC
+   fi
+
+   if [ -n "$LESSCHARSET" ]; then
+       export LESSCHARSET
+   elif [ "$TERM" = "linux-lat" ]; then
+       LESSCHARSET=latin1
+      export LESSCHARSET
+   fi
+
+
 fi
