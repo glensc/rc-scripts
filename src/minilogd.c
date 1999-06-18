dd4d6017cde95d17a4fd8baa32465fce401146bc
Arkadiusz Miśkiewicz <arekm@maven.pl>
1999-06-18 19:23:52 +0000

    pare zmian
    git-svn-id: http://svn.pld-linux.org/svn/rc-scripts/trunk@374 11da6c6d-24c7-0310-97e0-b6d17e038e62

---
+++ src/minilogd.c
@@ -0,0 +1,161 @@
+
+/* minilogd.c
+ * 
+ * A pale imitation of syslogd. Most notably, doesn't write anything
+ * anywhere except possibly back to syslogd.
+ * 
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <unistd.h>
+
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+
+static int we_own_log=0;
+static char **buffer=NULL;
+static int buflines=0;
+
+int debug;
+
+void freeBuffer() {
+   struct sockaddr_un addr;
+   int sock;
+   int x=0,conn;
+   
+   bzero(&addr,sizeof(addr));
+   addr.sun_family = AF_LOCAL;
+   strncpy(addr.sun_path,_PATH_LOG,sizeof(addr.sun_path)-1);
+   /* wait for klogd to hit syslog */
+   sleep(1);
+   sock = socket(AF_LOCAL, SOCK_STREAM,0);
+   conn=connect(sock,(struct sockaddr *) &addr,sizeof(addr));
+   while (x<buflines) {
+      if (!conn) write(sock,buffer[x],strlen(buffer[x])+1);
+      free(buffer[x]);
+      x++;
+   }
+}
+
+void cleanup(int exitcode) {
+   /* If we own the log, unlink it before trying to free our buffer.
+    * Otherwise, sending the buffer to /dev/log doesn't make much sense.... */
+   if (we_own_log) {
+      perror("wol");
+      unlink(_PATH_LOG);
+   }
+   /* Don't try to free buffer if we were called from a signal handler */
+   if (exitcode<=0) {
+       if (buffer) freeBuffer();
+       exit(exitcode);
+   } else
+      exit(exitcode+128);
+}
+
+void runDaemon(int sock) {
+   struct sockaddr_un addr;
+   int x,len,addrlen,recvsock,done=0;
+   char *message;
+   struct stat s1,s2;
+   struct pollfd pfds;
+    
+    daemon(0,-1);
+    /* try not to leave stale sockets lying around */
+    /* Hopefully, we won't actually get any of these */
+    signal(SIGHUP,cleanup);
+    signal(SIGINT,cleanup);
+    signal(SIGQUIT,cleanup);
+    signal(SIGILL,cleanup);
+    signal(SIGABRT,cleanup);
+    signal(SIGFPE,cleanup);
+    signal(SIGSEGV,cleanup);
+    signal(SIGPIPE,cleanup);
+    signal(SIGBUS,cleanup);
+    signal(SIGTERM,cleanup);
+   done = 0;
+   /* Get stat info on /dev/log so we can later check to make sure we
+    * still own it... */
+   stat(_PATH_LOG,&s1);
+   while (!done) {
+      pfds.fd = sock;
+      pfds.events = POLLIN|POLLPRI;
+      if ( ( (x=poll(&pfds,1,500))==-1) && errno !=EINTR) {
+	 perror("poll");
+	 cleanup(-1);
+      }
+      if ( (x>0) && pfds.revents & (POLLIN | POLLPRI)) {
+	 message = calloc(8192,sizeof(char));
+	 recvsock = accept(sock,(struct sockaddr *) &addr, &addrlen);
+	 len = read(recvsock,message,8192);
+	 if (buffer)
+	   buffer = realloc(buffer,(buflines+1)*sizeof(char *));
+	 else
+	   buffer = malloc(sizeof(char *));
+	 if (len>0) {
+	     message[strlen(message)]='\n';
+	    buffer[buflines]=message;
+	    buflines++;
+	    close(recvsock);
+	 }
+	 else {
+	    close(recvsock);
+	    recvsock=-1;
+	 }
+      }
+      if ( (x>0) && ( pfds.revents & (POLLHUP | POLLNVAL)) )
+	done = 1;
+      /* Check to see if syslogd's yanked our socket out from under us */
+      if ( (stat(_PATH_LOG,&s2)!=0) ||
+	    (s1.st_ino != s2.st_ino ) || (s1.st_ctime != s2.st_ctime) ||
+	    (s1.st_mtime != s2.st_mtime) || (s1.st_atime != s2.st_atime) ) {
+	 done = 1;
+	 we_own_log = 0;
+      }
+   }
+   cleanup(0);
+}
+
+int main(int argc, char **argv) {
+   struct sockaddr_un addr;
+   int sock;
+   int pid;
+    
+   /* option processing made simple... */
+   if (argc>1) debug=1;
+   /* just in case */
+   sock = open("/dev/null",O_RDWR);
+   dup2(sock,0);
+   dup2(sock,1);
+   dup2(sock,2);
+	
+   bzero(&addr, sizeof(addr));
+   addr.sun_family = AF_LOCAL;
+   strncpy(addr.sun_path,_PATH_LOG,sizeof(addr.sun_path)-1);
+   sock = socket(AF_LOCAL, SOCK_STREAM,0);
+   unlink(_PATH_LOG);
+   /* Bind socket before forking, so we know if the server started */
+   if (!bind(sock,(struct sockaddr *) &addr, sizeof(addr))) {
+      we_own_log = 1;
+      listen(sock,5);
+      if ((pid=fork())==-1) {
+	 perror("fork");
+	 exit(3);
+      }
+      if (pid) {
+	 exit(0);
+      } else {
+	  runDaemon(sock);
+	  /* shouldn't get back here... */
+	  exit(4);
+      }
+   } else {
+      exit(5);
+   }
+}
---
+++ src/process.c
@@ -0,0 +1,223 @@
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/poll.h>
+#include <sys/wait.h>
+
+#include <popt.h>
+
+#include "initlog.h"
+#include "process.h"
+
+int forkCommand(char **args, int *outfd, int *errfd, int *cmdfd, int quiet) {
+   /* Fork command 'cmd', returning pid, and optionally pointer
+    * to open file descriptor fd */
+    int fdin, fdout, fderr, fdcmd, pid;
+    int outpipe[2], errpipe[2], fdpipe[2];
+    
+    if ( (pipe(outpipe)==-1) || (pipe(errpipe)==-1) || (pipe(fdpipe)==-1) ) {
+	perror("pipe");
+	return -1;
+    }
+    
+    fdin=dup(0);
+    if (outfd) {
+       fdout = outpipe[1];
+      *outfd = outpipe[0];
+    } else {
+       if (!quiet)
+	 fdout=dup(1);
+    }
+    if (errfd) {
+       fderr = errpipe[1];
+      *errfd = errpipe[0];
+    } else {
+       if (!quiet)
+	 fderr=dup(2);
+    }
+    fdcmd = fdpipe[1];
+    if (cmdfd)
+      *cmdfd = fdpipe[0];
+    if ((pid = fork())==-1) {
+	perror("fork");
+	return -1;
+    }
+    if (pid) {
+	/* parent */
+	close(fdin);
+	close(fdout);
+	close(fderr);
+	close(fdcmd);
+	return pid;
+    } else {
+	/* kid */
+       if (outfd) { 
+	 if ( (dup2(fdout,1)==-1) ) {
+	    perror("dup2");
+	    exit(-1);
+	 }
+       } else if (quiet)
+	    if ((dup2(open("/dev/null",O_WRONLY),1))==-1) {
+	     perror("dup2");
+	     exit(-1);
+	    }
+
+       if (errfd)  {
+	 if ((dup2(fderr,2)==-1)) {
+	    perror("dup2");
+	    exit(-1);
+	 }
+       } else if (quiet) 
+	    if ((dup2(open("/dev/null",O_WRONLY),2))==-1)  {
+	       perror("dup2");
+	       exit(-1);
+	    }
+
+ 
+       if ((dup2(fdcmd,CMD_FD)==-1)) {
+	    perror("dup2");
+	    exit(-1);
+	}
+	close(fdout);
+	close(fderr);
+	close(fdcmd);
+	if (outfd)
+	  close(*outfd);
+	if (errfd)
+	  close(*errfd);
+	if (cmdfd)
+	  close(*cmdfd);
+	execvp(args[0],args);
+	perror("execvp");
+	exit(-1);
+    }
+}
+
+int monitor(char *cmdname, int pid, int numfds, int *fds, int reexec, int quiet, int debug) {
+    struct pollfd *pfds;
+    char *buf=malloc(2048*sizeof(char));
+    int outpipe[2];
+    char *tmpstr=NULL;
+    int x,y,rc=-1;
+    int done=0;
+    int output=0;
+    
+    pipe(outpipe);
+   
+    pfds = malloc(numfds*sizeof(struct pollfd));
+    for (x=0;x<numfds;x++) {
+	pfds[x].fd = fds[x];
+	pfds[x].events = POLLIN | POLLPRI;
+    }
+	
+    while (!done) {
+       if (((x=poll(pfds,numfds,500))==-1)&&errno!=EINTR) {
+	  perror("poll");
+	  return -1;
+       }
+       if (waitpid(pid,&rc,WNOHANG))
+	 done=1;
+       y=0;
+       while (y<numfds) {
+	  if ( x && ((pfds[y].revents & (POLLIN | POLLPRI)) )) {
+	     int bytesread = 0;
+	     
+	     do {
+		buf=calloc(2048,sizeof(char));
+		bytesread = read(pfds[y].fd,buf,2048);
+		if (bytesread==-1) {
+		   perror("read");
+		   return -1;
+		}
+		if (bytesread) {
+		  if (!quiet && !reexec)
+		    write(1,buf,bytesread);
+		  if (quiet) {
+		     output = 1;
+		     write(outpipe[1],buf,bytesread);
+		  }
+		  while ((tmpstr=getLine(&buf))) {
+		     if (!reexec) {
+			 if (getenv("IN_INITLOG")) {
+			     char *buffer=calloc(2048,sizeof(char));
+			     DDEBUG("sending =%s= to initlog parent\n",tmpstr);
+			     snprintf(buffer,2048,"-n %s -s \"%s\"\n",
+				      cmdname,tmpstr);
+			     write(CMD_FD,buffer,strlen(buffer));
+			     free(buffer);
+			 } else {
+			     logString(cmdname,tmpstr);
+			 }
+		     } else {
+			char **cmdargs=NULL;
+			char **tmpargs=NULL;
+			int cmdargc,x;
+			
+			poptParseArgvString(tmpstr,&cmdargc,&tmpargs);
+			cmdargs=malloc( (cmdargc++) * sizeof(char *) );
+			cmdargs[0]=strdup("initlog");
+			for (x=0;x<(cmdargc-1);x++) {
+			   cmdargs[x+1]=tmpargs[x];
+			}
+			processArgs(cmdargc,cmdargs,1);
+		     }
+		  }
+		}
+	        
+	     } while ( bytesread==2048 );
+	  }
+	  y++;
+       }
+    }
+    if ((!WIFEXITED(rc)) || (rc=WEXITSTATUS(rc))) {
+      /* If there was an error and we're quiet, be loud */
+      int x;
+      
+      if (quiet && output) {
+	 buf=calloc(2048,sizeof(char));
+	 do {
+	    x=read(outpipe[0],buf,2048);
+	    write(1,"\n",1);
+	    write(1,buf,x);
+	    buf=calloc(2048,sizeof(char));
+	 } while (x==2048);
+      }
+      return (rc);
+   }
+   return 0;
+}
+
+int runCommand(char *cmd, int reexec, int quiet, int debug) {
+    int fds[2];
+    int pid,x;
+    char **args, **tmpargs;
+    char *cmdname;
+    
+    poptParseArgvString(cmd,&x,&tmpargs);
+    args = malloc((x+1)*sizeof(char *));
+    for ( pid = 0; pid < x ; pid++) {
+	args[pid] = strdup(tmpargs[pid]);
+    }
+    args[pid] = NULL;
+    if (strcmp(args[0],"sh") && strcmp(args[0],"/bin/sh")) 
+      cmdname = basename(args[0]);
+    else
+      cmdname = basename(args[1]);
+    if ((cmdname[0] =='K' || cmdname[0] == 'S') && ( 30 <= cmdname[1] <= 39 )
+       && ( 30 <= cmdname[2] <= 39 ) )
+      cmdname+=3;
+    if (!reexec) {
+       pid=forkCommand(args,&fds[0],&fds[1],NULL,quiet);
+       x=monitor(cmdname,pid,2,fds,reexec,quiet,debug);
+    } else {
+       setenv("IN_INITLOG","yes",1);
+       pid=forkCommand(args,NULL,NULL,&fds[0],quiet);
+       unsetenv("IN_INITLOG");
+       x=monitor(cmdname,pid,1,&fds[0],reexec,quiet,debug);
+    }
+    return x;
+}
