6c9251de630fa2a34a472b1ea400ee926cc5098e
PLD Team <feedback@pld-linux.org>
2003-09-13 11:15:25 +0000

    This commit was manufactured by cvs2svn to create tag 'rc-scripts-0_0_4'.
    git-svn-id: http://svn.pld-linux.org/svn/rc-scripts/tags/rc-scripts-0_0_4@1140 11da6c6d-24c7-0310-97e0-b6d17e038e62

--- src/doexec.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-
-int main(int argc, char ** argv) {
-    execvp(argv[1], argv + 2);
-    return 1;
-}
--- src/testd.c
+++
@@ -1,7 +0,0 @@
-#include <sys/signal.h>
-#include <unistd.h>
-
-void main() {
-    signal(SIGTERM, SIG_IGN);
-    while (1) sleep(20);
-}
