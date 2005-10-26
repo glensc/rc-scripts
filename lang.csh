47618fca6b0fe60cd73a69abf3935b4ceca924d1
Jan Rękorajski <baggins@pld-linux.org>
2005-10-26 16:57:25 +0000

    - more white space and formatting cleanup
    git-svn-id: http://svn.pld-linux.org/svn/rc-scripts/trunk@6492 11da6c6d-24c7-0310-97e0-b6d17e038e62

--- lang.csh
+++ lang.csh
@@ -5,35 +5,30 @@
 
 test -f /etc/sysconfig/i18n
 if ($status == 0) then
-
-    if ($?LANG) then
-	setenv LANG
-    endif
-
-    if ($?LC_ALL) then
-	setenv LC_ALL
-    endif
-  
-    if ($?LINGUAS) then
-	setenv LINGUAS
-    endif
-  
-    if ($?SYSTERM) then
-	setenv TERM=$SYSTERM
-    endif
-
-    if ($?_XKB_CHARSET) then
-    	setenv _XKB_CHARSET
-    endif
-
-    if ($?INPUTRC) then
-    	setenv INPUTRC
-    endif
-
-    if ($?LESSCHARSET) then
-    	setenv LESSCHARSET
-    else if ($TERM == linux-lat) then
-    	setenv LESSCHARSET latin1
-    endif
-
+	if ($?LANG) then
+		setenv LANG
+	endif
+	if ($?LC_ALL) then
+		setenv LC_ALL
+	endif
+	if ($?LINGUAS) then
+		setenv LINGUAS
+	endif
+	if ($?SYSTERM) then
+		setenv TERM=$SYSTERM
+	endif
+	if ($?_XKB_CHARSET) then
+		setenv _XKB_CHARSET
+	endif
+	if ($?INPUTRC) then
+		setenv INPUTRC
+	endif
+	if ($?LESSCHARSET) then
+		setenv LESSCHARSET
+	else if ($TERM == linux-lat) then
+		setenv LESSCHARSET latin1
+	endif
 endif
+
+# This must be last line !
+# vi:syntax=csh
--- lang.sh
+++ lang.sh
@@ -4,34 +4,37 @@
 #
 
 if [ -f /etc/sysconfig/i18n ]; then
-    . /etc/sysconfig/i18n
+	. /etc/sysconfig/i18n
 
-   [ -n "$LANG" ] && export LANG || unset LANG
-   [ -n "$LC_CTYPE" ] && export LC_CTYPE || unset LC_CTYPE
-   [ -n "$LC_COLLATE" ] && export LC_COLLATE || unset LC_COLLATE
-   [ -n "$LC_MESSAGES" ] && export LC_MESSAGES || unset LC_MESSAGES
-   [ -n "$LC_NUMERIC" ] && export LC_NUMERIC || unset LC_NUMERIC
-   [ -n "$LC_MONETARY" ] && export LC_MONETARY || unset LC_MONETARY
-   [ -n "$LC_TIME" ] && export LC_TIME || unset LC_TIME
-   [ -n "$LC_ALL" ] && export LC_ALL || unset LC_ALL
-   [ -n "$LANGUAGE" ] && export LANGUAGE || unset LANGUAGE
-   [ -n "$LINGUAS" ] && export LINGUAS || unset LINGUAS
+	[ -n "$LANG" ] && export LANG || unset LANG
+	[ -n "$LC_CTYPE" ] && export LC_CTYPE || unset LC_CTYPE
+	[ -n "$LC_COLLATE" ] && export LC_COLLATE || unset LC_COLLATE
+	[ -n "$LC_MESSAGES" ] && export LC_MESSAGES || unset LC_MESSAGES
+	[ -n "$LC_NUMERIC" ] && export LC_NUMERIC || unset LC_NUMERIC
+	[ -n "$LC_MONETARY" ] && export LC_MONETARY || unset LC_MONETARY
+	[ -n "$LC_TIME" ] && export LC_TIME || unset LC_TIME
+	[ -n "$LC_ALL" ] && export LC_ALL || unset LC_ALL
+	[ -n "$LANGUAGE" ] && export LANGUAGE || unset LANGUAGE
+	[ -n "$LINGUAS" ] && export LINGUAS || unset LINGUAS
 
-   [ -n "$_XKB_CHARSET" ] && export _XKB_CHARSET || unset _XKB_CHARSET
+	[ -n "$_XKB_CHARSET" ] && export _XKB_CHARSET || unset _XKB_CHARSET
 
-   # deprecated
-   if [ -n "$SYSTERM" ]; then
-      export TERM=$SYSTERM
-   fi
+	# deprecated
+	if [ -n "$SYSTERM" ]; then
+		export TERM=$SYSTERM
+	fi
 
-   if [ -n "$INPUTRC" ]; then
-      export INPUTRC
-   fi
+	if [ -n "$INPUTRC" ]; then
+		export INPUTRC
+	fi
 
-   if [ -n "$LESSCHARSET" ]; then
-       export LESSCHARSET
-   elif [ "$TERM" = "linux-lat" ]; then
-       LESSCHARSET=latin1
-      export LESSCHARSET
-   fi
+	if [ -n "$LESSCHARSET" ]; then
+		export LESSCHARSET
+	elif [ "$TERM" = "linux-lat" ]; then
+		LESSCHARSET=latin1
+		export LESSCHARSET
+	fi
 fi
+
+# This must be last line !
+# vi:syntax=sh
